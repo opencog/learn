4f7ea9a7d88df3ee2a1c2afd72148193770d5318
Linas Vepstas <linasvepstas@gmail.com>
2019-06-12 18:05:37 -0500

    Provide the linear-programming variant.

--- scm/gram-class.scm
+++ scm/gram-class.scm
@@ -270,6 +270,41 @@
 ;
 ; Linear Programming merge
 ; ------------------------
+; Here, one searches for a vector `s` that maximizes some some
+; (information-theoretic) criterion for merging. This criterion takes
+; the form of a set of real numbers {a(d) |d is a disjunct} so that
+; N(s,d) = a(d) N(w,d). To obey non-negativity, one must have
+; 0 =< a(d) =< 1 for each `d`. This turns the problem into a linear
+; programming problem (or rather, a convex optimization problem).
+; If a(d) is either zero, or one, then this is a (binary) integer
+; programming problem.
+;
+; In the current context, there are two information-theoretic criteria
+; that seem reasonable to pursue. One is to maximize
+;
+;     S = MI(g,s) - MI(g,t)
+;
+; with MI as defined above. That is, we want to decompose `w=s+t` such
+; that the `s` component has the greatest possible MI with `g`, and the
+; remainder has the least-possible.
+;
+; A second possibility is to maximize
+;
+;     H = p(s) MI(g,w) - p(t) MI(g,t)
+;
+; where p(w) = dot(w,*) / dot(*,*) with dot(,) as defined above.
+;
+; In principle, integer optimization problems are NP-hard (NP-complete).
+; It is not entirely clear if that is the case here. There seems to be
+; at least one perhaps-hacky-but-linear-time algo:
+;  1. Create a sorted list of disjuncts `d` according to N(*,d)
+;  2. Create the empty set S
+;  3. For each `d`, from highest N(*,d) to lowest, create a vector `s`
+;     setting N(s,b)=N(w,b) if b is in S or if b==d; else N(s,b)=0.
+;  4. If the vector `s` is accepted by the criteria (i.e. it is larger)
+;     then define S = S union d.
+;  5. Loop to step 3 until done.
+;
 ;
 ; Formal definition
 ; -----------------
