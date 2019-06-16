85a43cbac66ced242157bdc9a3e3409c66b138cc
Linas Vepstas <linasvepstas@gmail.com>
2019-06-15 19:02:36 -0500

    Start actual comparisons

--- scm/compare.scm
+++ scm/compare.scm
@@ -12,8 +12,40 @@
 
 (LgDictNode "en")
 
-(cog-execute!
-(LgParseLink
-	(PhraseNode "this is a test.")
-	(LgDictNode "en")
-	(NumberNode 1)))
+
+
+(define (lg-compare en-dict other-dict SENT)
+"
+  lg-compare
+  SENT should be a string sentence
+
+"
+	; Get a parse, one for each dictionary.
+	(define en-sent (cog-execute!
+		(LgParseLink (PhraseNode SENT) en-dict (NumberNode 1))))
+	(define other-sent (cog-execute!
+		(LgParseLink (PhraseNode SENT) other-dict (NumberNode 1))))
+
+	; Since only one parse, we expect only one...
+	(define en-parse (gar (car
+		(cog-incoming-by-type en-sent 'ParseLink))))
+	(define other-parse (gar (car
+		(cog-incoming-by-type other-sent 'ParseLink))))
+
+	; Get a list of the words in each parse.
+	(define en-word-inst-list
+		(cog-incoming-by-type en-parse 'WordInstanceLink))
+
+	(define other-word-inst-list
+		(cog-incoming-by-type other-parse 'WordInstanceLink))
+
+	(define ewlilen (length en-word-inst-list))
+	(define owlilen (length other-word-inst-list))
+
+	; Both should have the same length, assuming the clever
+	; English tokenizer hasn't tripped.
+	(if (not (eq? ewlilen owlilen))
+		(format #t "Length miscompare: ~A vs ~A" ewlilen owlilen))
+
+	; The sequence of words should compare.
+)
