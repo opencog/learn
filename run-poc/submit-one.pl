d7dd7a1be959fa7d1dba99a1eaafee6724e9b74b
andres <suarezandres@gmail.com>
2019-06-21 19:19:31 +0800

    revert use of socket to send to cogserver; back to netcat

--- run-poc/submit-one.pl
+++ run-poc/submit-one.pl
@@ -5,7 +5,12 @@
 # Submit a collection of sentences, one sentence at a time, to the
 # cogserver located on host ARGV[0] and port ARGV[1].  The sentences
 # are read from standard input, and must be arranged with one sentence
-# per line. They are sent to the cogserver using ARGV[2] as the command.
+# per line. 
+# For the "file" mode, where word-instance-pair weights are read from a file, 
+# the input must be arranged in blocks starting with 
+# one sentence, and followed by the word-pairs and their weights, one pair
+# per line.  
+# They are sent to the cogserver using ARGV[2] as the command.
 # For word-pair counting, ARGV[2] is "observe-text-mode"
 # For disjunct counting, ARGV[2] is "observe-mst-mode"
 # ARGV[3] and ARGV[4] (and ARGV[5] for observe-mst-mode) are necessary
@@ -25,45 +30,45 @@ die "Wrong number of args!" if ($#ARGV < 4);
 die "Netcat failed! Bad host or port?" if (0 != $?);
 my $netcat = "|nc -N $ARGV[0] $ARGV[1]";
 
-use Socket;
-my $port =  "$ARGV[1]";
-my $server = "$ARGV[0]";
+# use Socket;
+# my $port =  "$ARGV[1]";
+# my $server = "$ARGV[0]";
 
-# Don't use netcat; just send directly
-# argument 1: what to send (ascii string)
-# argument 2: bool flag, wait for response, or not.
-sub send_stuff {
-	socket(SOCKET, PF_INET, SOCK_STREAM, (getprotobyname('tcp'))[2])
-		or die "Can't create a socket $!\n";
+# # Don't use netcat; just send directly
+# # argument 1: what to send (ascii string)
+# # argument 2: bool flag, wait for response, or not.
+# sub send_stuff {
+# 	socket(SOCKET, PF_INET, SOCK_STREAM, (getprotobyname('tcp'))[2])
+# 		or die "Can't create a socket $!\n";
 
-	connect(SOCKET, pack_sockaddr_in($port, inet_aton($server)))
-		or die "Can't connect to port $port! \n";
+# 	connect(SOCKET, pack_sockaddr_in($port, inet_aton($server)))
+# 		or die "Can't connect to port $port! \n";
 
-	my $text = $_[0];
+# 	my $text = $_[0];
 
-	# if subroutine has second argument, use it.
-	my $wait_for_response = 0;
-	if (1 < scalar @_) {
-		$wait_for_response = $_[1];
-	}
-	# print "Will wait for response: $wait_for_response\n";
+# 	# if subroutine has second argument, use it.
+# 	my $wait_for_response = 0;
+# 	if (1 < scalar @_) {
+# 		$wait_for_response = $_[1];
+# 	}
+# 	# print "Will wait for response: $wait_for_response\n";
 
-	if ($wait_for_response) {
-		SOCKET->autoflush(1);
-		print SOCKET "$text\n.\n.\n";
-		my $line;
-		while ($line = <SOCKET>) {
-			# print "Drained: $line";
-		}
-		close SOCKET;
-	} else {
-		print SOCKET "$text\n.\n.\n";
-		close SOCKET;
-	}
-}
+# 	if ($wait_for_response) {
+# 		SOCKET->autoflush(1);
+# 		print SOCKET "$text\n.\n.\n";
+# 		my $line;
+# 		while ($line = <SOCKET>) {
+# 			# print "Drained: $line";
+# 		}
+# 		close SOCKET;
+# 	} else {
+# 		print SOCKET "$text\n.\n.\n";
+# 		close SOCKET;
+# 	}
+# }
 
 my $start_time = time();
-my $sent_nbr = 0;
+# my $sent_nbr = 0;
 
 if ($ARGV[3] eq "file")
 {
@@ -72,8 +77,10 @@ if ($ARGV[3] eq "file")
 	{
 		if (/^\n$/) {
 			chomp($buffer);
-			$sent_nbr += 1;
-			send_stuff("($ARGV[2] \"$buffer\" \"$sent_nbr\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n");
+			open NC, $netcat || die "nc failed: $!\n";
+			print NC "($ARGV[2] \"$buffer\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n";
+			# $sent_nbr += 1;
+			# send_stuff("($ARGV[2] \"$buffer\" \"$sent_nbr\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n");
 			my $elapsed = time() - $start_time;
 			print "submit-one (elapsed $elapsed): $_\n";
 			$buffer="";
@@ -91,16 +98,20 @@ else
 		if (/^\n$/) { next; }
 		chop;
 
+ 		open NC, $netcat || die "nc failed: $!\n";
+
 		if ( $ARGV[2] eq "observe-text-mode" )
-			{open NC, $netcat || die "nc failed: $!\n";
- 			print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n"; }
+			{ print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n"; }
+			# {open NC, $netcat || die "nc failed: $!\n";
+ 			# print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n"; }
 			#{ send_stuff("($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n"); }
 		elsif ( $ARGV[2] eq "observe-mst-mode" )
-			{ $sent_nbr += 1;
-			send_stuff("($ARGV[2] \"$_\" \"$sent_nbr\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n"); }
+			{ print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n"; }
+			#{ $sent_nbr += 1;
+			#send_stuff("($ARGV[2] \"$_\" \"$sent_nbr\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n"); }
 		my $elapsed = time() - $start_time;
 		print "submit-one (elapsed $elapsed): $_\n";
 	}
 }
-sleep 5 ; # Small delay to allow socket processes to finish
+# sleep 5 ; # Small delay to allow socket processes to finish
 print "Done with article.\n";
