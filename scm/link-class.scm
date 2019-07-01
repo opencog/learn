47e0de2cb4eeb132f03c95abde1503dbe2199abe
Linas Vepstas <linasvepstas@gmail.com>
2019-07-01 11:33:11 -0500

    More counting

--- scm/link-class.scm
+++ scm/link-class.scm
@@ -1,11 +1,59 @@
 
 
-;; Just count how many disjuncts connect to some word.
-(define (count-half-links WORD-LST)
+;; Just count how many disjuncts connect to some word, either to
+;; the left or to the right. WORD-LST should be a list of WordNodes.
+;; This just gets the two connectors that WordNode might appear in,
+;; and then counts the number of ConnectorSeq's the connector appears
+;; in. The counting is without multiplicity (i.e. if the connector
+;; appears twice in the sequence, it is counted only once.)
+(define (count-quarter-links WORD-LST)
 	(fold
 		(lambda (CTR cnt)
 			(+ cnt (cog-incoming-size-by-type CTR 'ConnectorSeq)))
 		0
+		; A list of all the connectors
 		(append-map
 			(lambda (WRD) (cog-incoming-by-type WRD 'Connector))
-			WORD-LST)))
+			WORD-LST))
+)
+
+; Count the number of half-links.
+(define (count-half-links WORD-LST)
+
+	; Return the number of Sections containing the connector CON
+	(define (num-sects-w-con CON)
+		(fold
+			(lambda (CONSEQ CNT)
+				(+ CNT (cog-incoming-size-by-type CONSEQ 'Section)))
+			0
+			(cog-incoming-by-type CON 'ConnectorSeq)))
+
+	(fold
+		(lambda (CON cnt)
+			(+ cnt (num-sects-w-con CON)))
+		0
+		; A list of all the connectors
+		(append-map
+			(lambda (WRD) (cog-incoming-by-type WRD 'Connector))
+			WORD-LST))
+)
+
+; Count the total number of links
+(define (count-links WORD-LST)
+	; A list of all right-pointing connectors
+	(define ri-con-list (filter-map
+		(lambda (WRD)
+			(define ctr (cog-link 'Connector WRD (ConnectorDir "+")))
+			(and (not (equal? ctr '())) ctr))
+		WORD-LST))
+
+	; Return a list of Sections containing the connector CON
+	(define (sects-w-con CON)
+		(append-map
+			(lambda (CONSEQ)
+				(cog-incoming-by-type CONSEQ 'Section))
+		(cog-incoming-by-type CON 'ConnectorSeq)))
+
+	; Return a filtered list of Sections 
+)
+
