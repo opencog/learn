5a20ddfa7871e9ddce4fe3ad6126b587aedfb717
Linas Vepstas <linasvepstas@gmail.com>
2019-07-01 12:30:51 -0500

    Finish the link-counting code.

--- scm/link-class.scm
+++ scm/link-class.scm
@@ -38,14 +38,21 @@
 			WORD-LST))
 )
 
-; Count the total number of links
+; Count the total number of links, that is, the total mumber of
+; symmetric links between words, where each has a half-link back
+; to the other.
 (define (count-links WORD-LST)
-	; A list of all right-pointing connectors
-	(define ri-con-list (filter-map
-		(lambda (WRD)
-			(define ctr (cog-link 'Connector WRD (ConnectorDir "+")))
-			(and (not (equal? ctr '())) ctr))
-		WORD-LST))
+	; Return a right-pointing connector for the word, if it exists,
+	; else return #f
+	(define (right-con WRD)
+		(define ctr (cog-link 'Connector WRD (ConnectorDir "+")))
+		(and (not (equal? ctr '())) ctr))
+
+	; Return a left-pointing connector for the word, if it exists,
+	; else return #f
+	(define (left-con WRD)
+		(define ctr (cog-link 'Connector WRD (ConnectorDir "-")))
+		(and (not (equal? ctr '())) ctr))
 
 	; Return a list of Sections containing the connector CON
 	(define (sects-w-con CON)
@@ -54,6 +61,39 @@
 				(cog-incoming-by-type CONSEQ 'Section))
 		(cog-incoming-by-type CON 'ConnectorSeq)))
 
-	; Return a filtered list of Sections 
-)
+	; Return a list of Sections that are right-half-links,
+	; containing the word LEFT-WRD in some right-pointing
+	; connector in the Section.  That is, these sections can
+	; only appear to the right of LEFT-WRD.
+	(define (right-halves LEFT-WRD)
+		(define rc (right-con LEFT-WRD))
+		(if rc (sects-w-con rc) '()))
 
+	; Return a list of WordNodes that are linked from the left.
+	; This list contains no duplicated entries.
+	(define (right-words LEFT-WRD)
+		(define word-set (make-atom-set))
+		(for-each word-set (map gar (right-halves LEFT-WRD)))
+		(word-set #f))
+
+	; Return a list of sections which contain LEFT-WRD on
+	; the left, and are able to form a link to some word
+	; on the right. This list may contain duplicates!
+	(define (linkables LEFT-WRD)
+		(filter
+			(lambda (SECT)
+				(equal? (gar SECT) LEFT-WRD))
+			(append-map sects-w-con
+				(filter-map left-con (right-words LEFT-WRD)))))
+
+	; As above, but without duplicates
+	(define (links LEFT-WRD)
+		(define link-set (make-atom-set))
+		(for-each link-set (linkables LEFT-WRD))
+		(link-set #f))
+
+	; Now count.
+	(fold
+		(lambda (WRD CNT) (+ CNT (length (links WRD))))
+		0 WORD-LST)
+)
