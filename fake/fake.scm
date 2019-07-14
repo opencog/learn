0d585c1b9ed73fb6b7b1c925994f5ea6c919c65e
Linas Vepstas <linasvepstas@gmail.com>
2019-07-14 14:22:42 -0500

    bug fix and add planning notes.

--- fake/fake.scm
+++ fake/fake.scm
@@ -12,7 +12,9 @@
 
 (define (make-word N)
 "
-  Return the N'th word in the vocabulary
+  make-word N - Return the N'th word in the vocabulary.
+
+  Uses 26 lower-case ascii chars only, starting at "a".
 "
 	; 97 is the ascii-table offset to lower-case a
 	(define let (string (integer->char (+ 97 (remainder (- N 1) 26)))))
@@ -25,11 +27,24 @@
 
 (define (create-vocab SIZE)
 "
-  Create a random list containing SIZE words.
+  create-vocab SIZE - Create a random list containing SIZE words.
 "
 	; Number of letters in a word, assuming ASCII 26 letters.
 	(define word-length
 		(inexact->exact (ceiling (/ (log SIZE) (log 26)))))
 
-	(list-tabulate SIZE make-word)
+	(list-tabulate SIZE (lambda (N) (make-word (+ N 1))))
 )
+
+; create word-classes -- assign words to classes with zipf distribution.
+; i.e. each word-class contains zipf words in it.
+
+; create connectors
+
+; create disjuncts
+
+; create sections
+
+; use sections to generate text. This is hard, actually.
+; Two ways: (1) generate random strings, let LG discard those
+; that do not parse. (2) something else... yow!
