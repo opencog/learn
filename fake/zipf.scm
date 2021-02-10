ab5f8ce80c259f5298764a502089cb9fdc9de722
Linas Vepstas <linasvepstas@gmail.com>
2021-02-09 23:20:31 -0600

    Scaffolding for the zipf random number generator

---
+++ fake/zipf.scm
@@ -0,0 +1,21 @@
+;
+; zipf.scm
+;
+; Zipf random distribution in scheme.
+;
+; This provides guile-specific scaffolding to anable using the
+; srfi-194 random number generator. This is a hack. This is only
+; needed because guile does not currently support srfi-194.
+;
+
+; Fake make-random-real-generator for guile.
+(define (make-random-real-generator lo hi)
+	(lambda ()
+		(+ (* (- hi lo) (random:uniform)) lo))
+)
+
+; Convert float to exact integer.
+(define (exact FLOAT) (inexact->exact (floor FLOAT)))
+
+; Load the actual implementation.
+(load "zipf-zri.scm")
