1de4476f7dce611225a4efc92386e6eca1ffa7f6
Linas Vepstas <linasvepstas@gmail.com>
2021-02-11 09:49:55 -0600

    module boilerplate

---
+++ fake/fake.scm
@@ -0,0 +1,12 @@
+;
+; Artificial grammar module.
+; Wraps up the assorted tools and scripts into one module.
+;
+(define-module (opencog nlp fake))
+
+; The files are loaded in pipeline order.
+; In general, the later files depend on definitions contained
+; in the earlier files.
+(load "fake/zipf-zri.scm")
+(load "fake/zipf.scm")
+(load "fake/random-dict.scm")
