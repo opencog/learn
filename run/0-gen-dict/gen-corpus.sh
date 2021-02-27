7957b106c4a5ddaba18f6d3e1158aaedba72b54b
Linas Vepstas <linasvepstas@gmail.com>
2021-02-26 21:51:30 -0600

    Use file base paths

--- run/0-gen-dict/gen-corpus.sh
+++ run/0-gen-dict/gen-corpus.sh
@@ -5,16 +5,21 @@
 # `cp -r /where/ever/link-grammar/data/gen /tmp/fake-lang`
 #
 # -----------
-# Generate a dictionary and move it into place
-guile -l 0-gen-dict/gen-dict.scm
-mv /tmp/4.0.dict /tmp/fake-lang
+# Location where files should go. Suggest /home/user/data
+BASE=/tmp
 
-link-generator -l /tmp/fake-lang -s 1 -c 150000 > /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 2 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 3 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 4 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 5 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 6 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 7 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 8 -c 150000 >> /tmp/corpus.txt
-link-generator -l /tmp/fake-lang -s 9 -c 150000 >> /tmp/corpus.txt
+# -----------
+# Generate a dictionary and move it into place
+guile -l gen-dict.scm
+
+mv $BASE/4.0.dict $BASE/fake-lang
+
+link-generator -l $BASE/fake-lang -s 1 -c 150000 > $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 2 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 3 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 4 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 5 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 6 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 7 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 8 -c 150000 >> $BASE/corpus.txt
+link-generator -l $BASE/fake-lang -s 9 -c 150000 >> $BASE/corpus.txt
