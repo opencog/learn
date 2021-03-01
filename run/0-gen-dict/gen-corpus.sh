82add320f40aaa901c2129d67ca984ed9dc5e1b6
Linas Vepstas <linasvepstas@gmail.com>
2021-03-01 11:47:02 -0600

    Mkae script more configurable

--- run/0-gen-dict/gen-corpus.sh
+++ run/0-gen-dict/gen-corpus.sh
@@ -6,21 +6,22 @@
 #
 # -----------
 # Location where files should go. Suggest /home/user/data
-BASE=/tmp
+DICT=/home/ubuntu/data/fake-lang
+CORP=/home/ubuntu/data/fake-corpus
 
 # -----------
 # Generate a dictionary and move it into place
 guile -l gen-dict.scm
 
-mv $BASE/4.0.dict $BASE/fake-lang
+mv $BASE/4.0.dict $DICT
 
 # Generate corpus files, containing sentences of different lengths.
-link-generator -l $BASE/fake-lang -s 1 -c 150000 > $BASE/corpus-1.txt
-link-generator -l $BASE/fake-lang -s 2 -c 150000 > $BASE/corpus-2.txt
-link-generator -l $BASE/fake-lang -s 3 -c 150000 > $BASE/corpus-3.txt
-link-generator -l $BASE/fake-lang -s 4 -c 150000 > $BASE/corpus-4.txt
-link-generator -l $BASE/fake-lang -s 5 -c 150000 > $BASE/corpus-5.txt
-link-generator -l $BASE/fake-lang -s 6 -c 150000 > $BASE/corpus-6.txt
-link-generator -l $BASE/fake-lang -s 7 -c 150000 > $BASE/corpus-7.txt
-link-generator -l $BASE/fake-lang -s 8 -c 150000 > $BASE/corpus-8.txt
-link-generator -l $BASE/fake-lang -s 9 -c 150000 > $BASE/corpus-9.txt
+link-generator -l $DICT -s 1 -c 150000 > $CORP/corpus-1.txt
+link-generator -l $DICT -s 2 -c 150000 > $CORP/corpus-2.txt
+link-generator -l $DICT -s 3 -c 150000 > $CORP/corpus-3.txt
+link-generator -l $DICT -s 4 -c 150000 > $CORP/corpus-4.txt
+link-generator -l $DICT -s 5 -c 150000 > $CORP/corpus-5.txt
+link-generator -l $DICT -s 6 -c 150000 > $CORP/corpus-6.txt
+link-generator -l $DICT -s 7 -c 150000 > $CORP/corpus-7.txt
+link-generator -l $DICT -s 8 -c 150000 > $CORP/corpus-8.txt
+link-generator -l $DICT -s 9 -c 150000 > $CORP/corpus-9.txt
