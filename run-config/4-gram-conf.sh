a87fa90a1157afd9394236d56148415ab6a9935c
Linas Vepstas <linasvepstas@gmail.com>
2021-03-12 13:55:05 -0600

    Harmonize port numbers

--- run-config/4-gram-conf.sh
+++ run-config/4-gram-conf.sh
@@ -6,7 +6,7 @@
 #
 # IPv4 hostname and port number of where the cogserver is running.
 export HOSTNAME=localhost
-export PORT=20010
+export PORT=19008
 export PROMPT="scheme@(gram-class)> "
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-fake.conf
 
