2d681bf3782443b1ef8423eb8d92f999b2811c7f
Linas Vepstas <linasvepstas@gmail.com>
2021-03-29 20:53:31 -0500

    More working code

--- run-common/marginals-mst-shape.scm
+++ run-common/marginals-mst-shape.scm
@@ -1,29 +1,39 @@
 #! /usr/bin/env -S guile
 !#
 ;
-; marginals-mst.scm
+; marginals-mst-shape.scm
 ;
 ; After disjunct counting has been completed, this script is run to
 ; compute marginal statistics. This computes assorted totals and
 ; conditional probabilities needed before disjunct clustering can be
 ; done.
 ;
+; This is a variant of the `marginals-mst.scm` script that uses the
+; "more accurate" (but larger and slower) shape vectors.
+;
 (load "cogserver.scm")
 
 ; Init the statistics objects.
 (define pca (make-pseudo-cset-api))
 (define psa (add-pair-stars pca))
-(define btr (batch-transpose psa))
+
+(define wsv (make-shape-vec-api))
+(define wss (add-pair-stars wsv))
+
+(define cac (direct-sum psa wss))
+(define csc (add-pair-stars cac))
+(csc 'fetch-pairs)
 
 ; Load up the word-pairs -- this can take over half an hour!
-(display "Fetch all disjuncts. This may take well over half-an-hour!\n")
-(psa 'fetch-pairs)
+(display "Fetch all shapes. This may take well over half-an-hour!\n")
+(csc 'fetch-pairs)
 
 ; Compute the matrix-transpose marginals. As a side-effect, this will
 ; also compute support marginals and the central sums.
+(define btr (batch-transpose csc))
 (btr 'mmt-marginals)
 
-(print-matrix-summary-report psa)
+(print-matrix-summary-report csc)
 
 (barrier storage-node)
 ; (cog-close storage-node)
