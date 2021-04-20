c599c856f59c1815b92520bee043c588d0d1ebf9
Linas Vepstas <linasvepstas@gmail.com>
2021-04-20 11:36:54 -0500

    typo

--- scm/gram-projective.scm
+++ scm/gram-projective.scm
@@ -561,7 +561,7 @@
 
 	; Return a WordClassNode that is the result of the merge.
 	(define (merge WA WB)
-		(define single (not (eq? (STARS 'cluster-type (cog-type WA))))
+		(define single (not (eq? (STARS 'cluster-type (cog-type WA)))))
 		(define cls (STARS 'make-cluster WA WB))
 
 		; Cluster - either create a new cluster, or add to an existing
