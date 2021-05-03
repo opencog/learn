d18c1605bfceeb0a293e1dec80b678cb00cf4f4e
Linas Vepstas <linasvepstas@gmail.com>
2021-05-02 22:53:44 -0500

    More unit testing

--- tests/connector-merge.scm
+++ tests/connector-merge.scm
@@ -29,21 +29,40 @@
 (define t-simple "simplest merge test")
 (test-begin t-simple)
 
-(load "connector-data.scm")
-
+; Open the database
 (setup-database)
+
+; Load some data
+(load "connector-data.scm")
 (setup-basic-sections)
 
+; Define matrix API to the data
 (define pca (make-pseudo-cset-api))
 (define csc (add-covering-sections pca))
-(csc 'explode-sections)
 (define gsc (add-cluster-gram csc))
 
+; Verify that the data loaded correctly
+; We expect 3 sections on "e" and two on "j"
+(test-equal 3 (length (gsc 'right-stars (Word "e"))))
+(test-equal 2 (length (gsc 'right-stars (Word "j"))))
+
+; Create CrossSections and verify that they got created
+(csc 'explode-sections)
+(test-equal 15 (cog-get-atoms 'CrossSection))
+
+; Merge two sections together.
 (define disc (make-discrim gsc 0.25 4 4))
 (disc 'merge-function (Word "e") (Word "j"))
 
+; We expect just one section left on "e", the klm section.
+(test-equal 1 (length (gsc 'right-stars (Word "e"))))
 
-(test-equal "foo" #t (not #f))
+; We expect no sections left on j
+(test-equal 0 (length (gsc 'right-stars (Word "j"))))
+
+; We expect three merged sections
+(test-equal 3 (length (gsc 'right-stars (WordClassNode "e j"))))
+
 (test-end t-simple)
 
 ; ---------------------------------------------------------------
