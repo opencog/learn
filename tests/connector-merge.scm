7748c473b2d1a436824cd50b3ecef06540092bf0
Linas Vepstas <linasvepstas@gmail.com>
2021-05-02 22:07:08 -0500

    Start getting serious abut unit test

--- tests/connector-merge.scm
+++ tests/connector-merge.scm
@@ -10,13 +10,60 @@
 (opencog-test-runner)
 
 ; ---------------------------------------------------------------
-(define tgra "sniff test")
-(test-begin tgra)
+; Some very simple test vectors
+(define (setup-basic-sections)
+	(Section (ctv 1 0 13)
+		(Word "e")
+		(ConnectorSeq
+			(Connector (Word "a") (ConnectorDir "-"))
+			(Connector (Word "b") (ConnectorDir "-"))
+			(Connector (Word "c") (ConnectorDir "+"))))
+	(Section (ctv 1 0 61)
+		(Word "e")
+		(ConnectorSeq
+			(Connector (Word "d") (ConnectorDir "-"))
+			(Connector (Word "g") (ConnectorDir "-"))
+			(Connector (Word "h") (ConnectorDir "+"))))
+	(Section (ctv 1 0 44)
+		(Word "e")
+		(ConnectorSeq
+			(Connector (Word "k") (ConnectorDir "-"))
+			(Connector (Word "l") (ConnectorDir "+"))
+			(Connector (Word "m") (ConnectorDir "+"))))
 
-(define x #t)
-(define y (not #f))
+	; Similar to above.
+	(Section (ctv 1 0 31)
+		(Word "j")
+		(ConnectorSeq
+			(Connector (Word "a") (ConnectorDir "-"))
+			(Connector (Word "b") (ConnectorDir "-"))
+			(Connector (Word "c") (ConnectorDir "+"))))
+	(Section (ctv 1 0 16)
+		(Word "j")
+		(ConnectorSeq
+			(Connector (Word "d") (ConnectorDir "-"))
+			(Connector (Word "g") (ConnectorDir "-"))
+			(Connector (Word "h") (ConnectorDir "+"))))
+	; (Section k l m) intintionally absent.
+)
 
-(test-equal "foo" x y)
-(test-end tgra)
+; ---------------------------------------------------------------
+(define t-simple "simplest merge test")
+(test-begin t-simple)
+
+(setup-basic-sections)
+(define pca (make-pseudo-cset-api))
+(define csc (add-covering-sections pca))
+(csc 'explode-sections)
+(define gsc (add-cluster-gram csc))
+
+(define disc (make-discrim gsc 0.25 4 4))
+(disc 'merge-function (Word "e") (Word "j"))
+
+
+
+
+(test-equal "foo" #t (not #f))
+(test-end t-simple)
 
 ; ---------------------------------------------------------------
