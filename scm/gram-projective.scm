069eaf23c677304b59dd3da431e880ba2200b307
Linas Vepstas <linasvepstas@gmail.com>
2021-05-08 09:32:46 -0500

    Refactor and do merge-into-cluster

--- scm/gram-projective.scm
+++ scm/gram-projective.scm
@@ -646,31 +646,21 @@ unfinished prototype
 
 			(define (do-acc CNT PR WEI)
 				(set! CNT (+ CNT
-						(accumulate-count LLOBJ mrg PR WEI NOISE))))
+					(accumulate-count LLOBJ mrg PR WEI NOISE)))
+				(if MRG-CON
+					(merge-crosses LLOBJ CLS PR WEI NOISE)))
 
 			; Now perform the merge. Overlapping entries are
 			; completely merged (frac=1.0). Non-overlapping ones
 			; contribute only FRAC.
 			(monitor-rate #f)
 			(cond
-				(null-a
-					(begin
-						(do-acc accum-bcnt PAIR-B frac-to-merge)
-						(if MRG-CON
-							(merge-crosses LLOBJ CLS PAIR-B frac-to-merge NOISE))))
-				(null-b
-					(begin
-						(do-acc accum-acnt PAIR-A frac-to-merge)
-						(if MRG-CON
-							(merge-crosses LLOBJ CLS PAIR-A frac-to-merge NOISE))))
+				(null-a (do-acc accum-bcnt PAIR-B frac-to-merge))
+				(null-b (do-acc accum-acnt PAIR-A frac-to-merge))
 				(else ; AKA (not (or null-a null-b))
 					(begin
 						(do-acc accum-acnt PAIR-A 1.0)
-						(do-acc accum-bcnt PAIR-B 1.0)
-						(when MRG-CON
-							(merge-crosses LLOBJ CLS PAIR-A 1.0 NOISE)
-							(merge-crosses LLOBJ CLS PAIR-B 1.0 NOISE)))))
-		)
+						(do-acc accum-bcnt PAIR-B 1.0)))))
 		perls)
 
 	(monitor-rate
@@ -780,7 +770,9 @@ unfinished prototype
 			(define (do-acc PRC WEI)
 				(monitor-rate #f)
 				(set! accum-cnt (+ accum-cnt
-						(accumulate-count LLOBJ PRC PAIR-A WEI NOISE))))
+					(accumulate-count LLOBJ PRC PAIR-A WEI NOISE)))
+				(if MRG-CON
+					(merge-crosses LLOBJ CLS PAIR-A WEI NOISE)))
 
 			; There's nothing to do if A is empty.
 			(when (not (null? PAIR-A))
@@ -803,8 +795,10 @@ unfinished prototype
 	(monitor-rate
 		"------ Extend: Merged ~A sections in ~5F secs; ~6F scts/sec\n")
 
+#! ==========
 	; Merge connectors, if asked to do so.
 	(when MRG-CON (merge-connectors LLOBJ CLS WA))
+========= !#
 
 	; Cleanup after merging.
 (define nda
