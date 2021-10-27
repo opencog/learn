3fe2f7f5fb3737c7c50aee5412330f5d756f9077
Linas Vepstas <linasvepstas@gmail.com>
2021-10-27 17:52:06 -0500

    Code has been ported to the generic matrix code base.

--- scm/gram-majority.scm
+++ scm/gram-majority.scm
@@ -46,74 +46,39 @@
 ;
 ; TODO: maybe reintroduce NOISE as well.
 
-(define-public (make-majority-jaccard LLOBJ QUORUM)
+(define-public (count-shared-conseq LLOBJ QUORUM WORD-LIST)
 "
-  make-majority-jaccard LLOBJ QUORUM -- Return a function that
-  counts the number of connector sequences shared by a majority
-  of the words in a word list. The majority is determined by QUORUM,
-  which should be a floating-point number between 0.0 and 1.0.
+  count-shared-conseq LLOBJ QUORUM WORD-LIST -- Return a count of the
+  number of connector sequences which are shared by a majority of the
+  words in WORD-LIST. Actually, return a list of two numbers: this
+  count and the total connector sequences appearing in common in all
+  of the words.  Dividing these two numbers gives a generalized form
+  of the Jaccard similarity between all of the words in the WORD-LIST.
 
-  When the returned function is invoked on a list of words, it returns
-  a list of two numbers: the number of connector sequences shared by
-  the majority, and the total number number of connector sequences that
-  appear on the words.
+  The majority is determined by QUORUM, which should be a floating-
+  point number between 0.0 and 1.0.
 
-  This returns a function that implements a kind-of Jaccard distance
-  between multiple words (two or more).  The conventional Jaccard
-  distance is defined only for pairs of items. The generalization is
-  done by counting to see if a fraction QUORUM is shared. Setting
-  QUORUM to 1.0, and appplying the function to two items returns
-  the conventional Jaccard distance.
+  This function is a kind-of Jaccard distance between multiple words
+  (two or more).  The conventional (unweighted) Jaccard distance is
+  defined only for pairs of items. The generalization is done by
+  counting to see if a fraction QUORUM is shared. Setting QUORUM to
+  1.0, and appplying the function to two items returns the conventional
+  Jaccard distance.
 "
-	; WLIST is a list of WordNodes and/or WordClassNodes that are
-	; being proposed for merger. This will count how many disjuncts
-	; these share in common. It returns a pair of numbers: the number
-	; of disjuncts shared, and the total disjuncts.
-	(define (count WLIST)
-
-		; The minimum number of sections that must exist for
-		; a given disjunct. For a list of length two, both
-		; must share that disjunct (thus giving the traditional
-		; overlap merge).
-		(define wlen (length WLIST))
-		(define vote-thresh
-			(if (equal? wlen 2) 2
-				(inexact->exact (round (* QUORUM wlen)))))
-
-		; Return #t if the DJ is shared by the majority of the
-		; sections. Does the count exceed the threshold?
-		(define (vote-to-accept? DJ)
-			(<= vote-thresh
-				(fold
-					(lambda (WRD CNT)
-						(if (nil? (LLOBJ 'get-pair WRD DJ)) CNT (+ 1 CNT)))
-					0
-					WLIST)))
-
-		; Put all of the connector-sets on all of the words int a bag.
-		(define set-of-all-djs (make-atom-set))
-		(for-each
-			(lambda (WRD)
-				(for-each
-					(lambda (DJ) (set-of-all-djs DJ))
-					(LLOBJ 'right-basis WRD)))
-			WLIST)
-
-		(define list-of-all-djs (set-of-all-djs #f))
-
-		; Count the particular DJ, if it is shared by the majority.
-		(define shared-count
-			(fold
-				(lambda (DJ CNT)
-					(if (vote-to-accept? DJ) (+ 1 CNT) CNT))
-				list-of-all-djs))
-
-		; Return two numbers: the shared count and the total count.
-		(list shared-count (length list-of-all-djs))
+	; The threshold, the minimum number of sections that must exist
+	; for a given disjunct. For a list of length two, both must share
+	; that disjunct (thus giving the traditional overlap merge).
+	(define wlen (length WLIST))
+	(define vote-thresh
+		(if (equal? wlen 2) 2
+			(inexact->exact (round (* QUORUM wlen)))))
 	)
 
-	; Return the above function
-	count
+	; The group-similarity function uses a strictly-less-than compare.
+	; So, to be shared by two, its enough to set low-bnd to one.
+	(define low-bnd (- vote-thresh 1))
+
+	((make-group-similarity LLOBJ) 'mutual-col-supp low-bnd WORD-LIST)
 )
 
 (define-public (make-merge-majority LLOBJ QUORUM MRG-CON)
