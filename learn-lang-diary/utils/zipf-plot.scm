f31ea3cc071148dd953dd2af6382d0d450473b47
Linas Vepstas <linasvepstas@gmail.com>
2022-02-12 19:11:04 -0600

    Add a zipf-ranking plot to part-six

--- learn-lang-diary/utils/zipf-plot.scm
+++ learn-lang-diary/utils/zipf-plot.scm
@@ -37,6 +37,27 @@
 			(format port "~A  ~A \"~A\"\n" cnt (car pr) (fn-str (cdr pr))))
 		scrs))
 
+; Same as above, but only print when the score has changed.
+(define (print-sparse-rank-fn scrs port fn-str)
+	(define cnt 0)
+	(define score 1e30)
+	(for-each
+		(lambda (pr)
+			(define iscor (inexact->exact (car pr)))
+			(set! cnt (+ cnt 1))
+			(when (< iscor score)
+				(set! score iscor)
+				(format port "~A  ~A \"~A\"\n" cnt iscor (fn-str (cdr pr)))
+				(force-output port)))
+		scrs)
+
+	; Print the very last one too.
+	(define final (car (take-right scrs 1)))
+	(format port "~A  ~A \"~A\"\n" (length scrs)
+		(inexact->exact (car final)) (fn-str (cdr final)))
+	(force-output port)
+)
+
 ; ---------------------------------------------------------------------
 ; Zipf graph from Diary Part Six
 
@@ -52,7 +73,8 @@
 		" <<>> " (cog-name (star-obj 'right-element ITEM))))
 
 (let ((outport (open-file "/tmp/pair-rank.dat" "w")))
-	(print-ts-rank-fn pair-list outport prt-pair)
+	; (print-ts-rank-fn pair-list outport prt-pair)
+	(print-sparse-rank-fn pair-list outport prt-pair)
 	(close outport))
 
 ; ---------------------------------------------------------------------
