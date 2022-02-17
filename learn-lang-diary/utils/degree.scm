078e4eb24b418cb6ca19219a321b1d4369d9a70b
Linas Vepstas <linasvepstas@gmail.com>
2022-02-16 20:37:57 -0600

    Graph log2 of degree
    OK, so now the hist is ...slope 1

--- learn-lang-diary/utils/degree.scm
+++ learn-lang-diary/utils/degree.scm
@@ -8,19 +8,30 @@
 ; support (this is the degree)
 ;
 
-(define (degree-plot LLOBJ NBINS MAX-DEG)
+(define (degree-plot LLOBJ NBINS LO HI FILE VALFN WEIFN)
 
 	(define lwords (LLOBJ 'left-basis))
 
 	(define sup-obj (add-support-api LLOBJ))
 
-	; For each left-word, the right-support is the "degree" of that node.
-	(define (pval WRD) (sup-obj 'right-support WRD))
-	(define (pcnt WRD) 1)
+	(define bins (bin-count lwords NBINS VALFN WEIFN LO HI))
 
-	(define bins (bin-count lwords NBINS pval pcnt 0 MAX-DEG))
-
-	(define oport (open-file "/tmp/foo.dat" "w"))
+	(define oport (open-file FILE "w"))
 	(print-bincounts-tsv bins oport)
 	(close oport)
 )
+
+(define (log2 x)
+	(if (< 0 x) (/ (log x) (log 2)) 0.0))
+
+; For each left-word, the right-support is the "degree" of that node.
+(define (pval WRD) (sup-obj 'right-support WRD))
+(define (pcnt WRD) 1)
+(degree-plot star-obj 200 0 50000 "/tmp/degree.dat" pval pcnt)
+
+; Same as above, but uniform x-scale
+(define (pval WRD) (log2 (sup-obj 'right-support WRD)))
+(define (pcnt WRD) 1)
+(degree-plot star-obj 200 0 20 "/tmp/log2-degree.dat" pval pcnt)
+
+
