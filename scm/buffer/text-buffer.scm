4b35c6973ee4304c90ca81a5f1bea5149855d8d3
Linas Vepstas <linasvepstas@gmail.com>
2022-11-14 16:18:23 -0600

    Foutz with the buffer

--- scm/buffer/text-buffer.scm
+++ scm/buffer/text-buffer.scm
@@ -18,19 +18,42 @@
 (use-modules (srfi srfi-1))
 
 ; Anchor for all text buffers in the system
-(define buffer-base (AnchorNode "*-Text Buffers-*"))
+(define *-buffer-base-* (AnchorNode "*-Text Buffers-*"))
 
 ; --------------------------------------------------------
 (define-public (buffer-open)
 "
   buffer-open -- Create a new buffer for reading and writing.
-"
 
+  Returns a TagNode Atom identifying the buffer stream.
+
+  See also:
+    buffer-append -- Add text to bufffer.
+"
 	(define buftag (random-node 'TagNode 16 "text"))
-	(TagLink buftag buffer-base)
+	(TagLink buftag *-buffer-base-*)
 
 	; Return the tag.
 	buftag
+)
+
+; --------------------------------------------------------
+
+(define-public (buffer-append TAGNAME TEXT)
+"
+  buffer-append TAGNAME TEXT-- append utf-8 TEXT to the TAGANAME buffer
+
+  Returns #t if sucessful, else #f.
+  Failure reasons include -- no such buffer, buffer not open.
+
+  See also:
+    buffer-open -- Create a new text buffer.
+"
+	(define buftag (cog-node 'TagNode TAGNAME))
+	(if (or (nil? buftag)
+			(nil? (cog-link 'TagLink buftag *-buffer-base-*))) #f
+		xx
+	)
 )
 
 ; --------------------------------------------------------
