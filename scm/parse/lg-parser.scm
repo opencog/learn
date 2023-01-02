ceb6953e97a8f50a094c4b7f68b10c4d2e32a2e0
Linas Vepstas <linasvepstas@gmail.com>
2023-01-01 22:54:21 -0600

    Use the local dict, if possible.

--- scm/parse/lg-parser.scm
+++ scm/parse/lg-parser.scm
@@ -8,32 +8,49 @@
 (use-modules (opencog) (opencog nlp) (opencog nlp lg-parse))
 (use-modules (opencog exec))
 
-(define*-public (make-disjunct-counter LLOBJ
+(define*-public (make-disjunct-counter LLOBJ DICT
 	#:key
 		(NUM-LINKAGES 6)
-		(DICT (LgDict "any"))
+		(ATOMSPACE #t)
+		(STORAGE #f))
 	)
 "
-  make-disjunct-counter LLOBJ --
-     Return a function that will update Section counts using LLOBJ.
+  make-disjunct-counter LLOBJ DICT -- Parse text using DICT.
+
+  Return a function that will parse text strings, and update Section
+  counts (using LLOBJ) for the parses. The LgDictNode DICT will be
+  used to access the dictionary.
 
   The LLOBJ should be a matrix object that holds Sections. Typically,
   the `make-pseudo-cset-api` object, or something similar, should be
   used.
+
+  The DICT should be an `LgDictNode` specifying the dictionary.
+  Recommend the one in `run-common/dict-combined`.
 
   This returns a function that takes a single argument, a plain-text
   UTF-8 string holding a single sentence, and sends it to the
   Link Grammar parser for parsing. The resulting parses are converted
   into Sections and given to the LLOBJ for counting.
 
-  This takes two optional parameters:
+  This takes three optional parameters:
 
-  #:NUM-LINKAGES -- the number of linkages that the LG parser should
+  #:NUM-LINKAGES -- The number of linkages that the LG parser should
   generate. Recall that each linkage is a different parse of the
   sentence; these are returned in cost-sorted order. Default is 6.
 
-  #:DICT -- the `LgDictNode` to use. This is the dictionary to use for
-  parsing. 
+  #:ATOMSPACE -- Use the provided AtomSpace for dictionary contents.
+  If not specified, use the current AtomSpace in the current thread.
+  Set this to #f to use a local, private AtomSpace. In this case, the
+  DICT must specify a StorageNode which can provide the dictionary
+  contents.
+
+  #:STORAGE -- If the current AtomSpace is being used, or a custom
+  AtomSpace has been specified, then this parameter can be any
+  StorageNode (including ProxyNodes) that will be used to access
+  dictionary data. This can be used to specify a ProxyNode that
+  computes MI or other statistics on-th-fly, for the accessed
+  dictionary definitions.
 "
 	(define NUML (Number NUM-LINKAGES))
 
@@ -50,12 +67,19 @@
 			(cog-value->list SECTL)))
 
 	(define (obs-txt PLAIN-TEXT)
+		; Where is the dictionary? If #t, use the local AS.
+		(define dict-as
+			(if ATOMSPACE
+				(if (cog-atom? ATOMSPACE) ATOMSPACE (cog-atomspace))))
+
 		; Do the parsing in a temp atomspace, and the counting in
 		; the base space. The temp space must remain until we are
 		; done counting, else the atoms will disappear.
 		(define base-as (cog-push-atomspace))
 		(define parses (cog-execute!
-			(LgParseSections (Phrase PLAIN-TEXT) DICT NUML)))
+			(LgParseSections (Phrase PLAIN-TEXT) DICT NUML
+				(if dict-as dict-as)
+				(if STORAGE STORAGE))))
 		(define temp-as (cog-set-atomspace! base-as))
 
 		(for-each update-section-counts (cog-value->list parses))
@@ -74,6 +98,6 @@
 ; (define dict (LgDictNode "run-config/dict-combined"))
 ; (define pca (make-pseudo-cset-api))
 ;
-; (define parser (make-disjunct-counter pca #:DICT dict))
+; (define parser (make-disjunct-counter pca dict))
 ; (parser "this is a test")
 ; ---------------------------------------------------------------------
