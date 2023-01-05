45d1b62a71ded2723bbe1b5790888b9fe9c54ba5
Linas Vepstas <linasvepstas@gmail.com>
2023-01-05 13:10:48 -0600

    Fix unexpected garbage in file.

--- run-common/marginals-pair.scm
+++ run-common/marginals-pair.scm
@@ -4,14 +4,7 @@
 ; marginals-pair.scm
 ;
 ; After word-pair counting has been completed, this script is run to
-; compute marginal statistics. This computes assorted totals and
-; conditional probabilities needed before the next step.
-;
-; A reasonable alternative to running this is running `trim-pairs.scm`
-; which will trim down the dataset, and then compute the marginals for
-; the smaller dataset. Except that maybe trimming is a bad idea.
-; Although the jury is still out on that. Trimming is important, but
-; subtle.
+; compute marginal probabilities and word-pair mutual information.
 ;
 ; ---------------------------------------------------------------------
 (load "cogserver.scm")
@@ -22,7 +15,8 @@
 (define asa (add-pair-stars aca))
 
 ; Load the data.
-(asa 'fetch-pairs)))
+(display "Start loading sparse matrix pairs\n")
+(asa 'fetch-pairs)
 (display "Finished loading sparse matrix pairs\n")
 (cog-report-counts)
 
