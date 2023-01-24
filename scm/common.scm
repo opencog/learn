48df24b23bd8fc583cf548ad9dd7554f75e3ee51
Linas Vepstas <linasvepstas@gmail.com>
2023-01-23 20:25:45 -0600

    Add a generic increment counter

--- scm/common.scm
+++ scm/common.scm
@@ -24,6 +24,38 @@
 
 ; ---------------------------------------------------------------------
 
+(define (count-inc-atom ATM CNT)
+"
+  count-inc-atom ATM CNT -- increment the count by CNT on ATM, and
+  update storage to hold that count.
+
+  This will also automatically fetch the previous count from storage,
+  so that counting will work correctly, when picking up from a previous
+  point.
+
+  Warning: this is NOT SAFE for distributed processing! That is
+  because this does NOT grab the count from the database every time,
+  so if some other process updates the database, this will miss that
+  update. Multiple distributed counters will continue to clobber
+  each-other indefinitely; the system will NOT settle-down long-term.
+
+  See also: count-one-atom
+"
+	(define (incr-one atom)
+		; If the atom doesn't yet have a count TV attached to it,
+		; then its probably a freshly created atom. Go fetch it
+		; from storage. Otherwise, assume that what we've got here,
+		; in the atomspace, is the current copy.  This works if
+		; there is only one process updating the counts.
+		(if (not (cog-ctv? (cog-tv atom)))
+			(fetch-atom atom)) ; get from storage
+		(cog-inc-count! atom INC) ; increment
+	)
+	(begin
+		(incr-one ATM) ; increment the count on ATM
+		(store-atom ATM)) ; save to storage
+)
+
 (define (count-one-atom ATM)
 "
   count-one-atom ATM -- increment the count by one on ATM, and
@@ -38,20 +70,10 @@
   so if some other process updates the database, this will miss that
   update. Multiple distributed counters will continue to clobber
   each-other indefinitely; the system will NOT settle-down long-term.
+
+  See also: count-inc-atom
 "
-	(define (incr-one atom)
-		; If the atom doesn't yet have a count TV attached to it,
-		; then its probably a freshly created atom. Go fetch it
-		; from storage. Otherwise, assume that what we've got here,
-		; in the atomspace, is the current copy.  This works if
-		; there is only one process updating the counts.
-		(if (not (cog-ctv? (cog-tv atom)))
-			(fetch-atom atom)) ; get from storage
-		(cog-inc-count! atom 1) ; increment
-	)
-	(begin
-		(incr-one ATM) ; increment the count on ATM
-		(store-atom ATM)) ; save to storage
+	(count-inc-atom ATM 1)
 )
 
 ; ---------------------------------------------------------------------
