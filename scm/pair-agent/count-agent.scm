a1ae4dd4567877f59aaffeb2d225772dbb865fae
Linas Vepštas <linasvepstas@gmail.com>
2024-04-03 15:47:40 -0500

    Remove crud

--- scm/pair-agent/count-agent.scm
+++ scm/pair-agent/count-agent.scm
@@ -7,48 +7,26 @@
 (define NUML (Number 4))
 (define DICT (LgDict "any"))
 
-; Same as always
 
 (define (obs-txt PLAIN-TEXT)
-	(define parses (cog-execute! (PureExecLink
-		(LgParseBonds (Phrase PLAIN-TEXT) DICT NUML))))
-
+	(define parses (cog-execute!
+		(Filter
+			(Signature
+				(Type 'LinkValue))
+			(PureExecLink (LgParseBonds (Phrase PLAIN-TEXT) DICT NUML)))))
 	parses
 )
 
-(use-modules (opencog) (opencog exec))
-(use-modules (opencog nlp) (opencog nlp lg-parse))
+;returns NUML pairs in list equal to SizeOf but SizeOf might be less
+;first in pair is wordlist, second is edgelist.
 
 
 ; Parses arrive as link values. We want to apply a function to each.
 ; How?
 ; Need:
 ; 1) execute in subspace DONE PureExecLink
-; 2) decimate w/ accept (for unbounded number of edges) NO
-; 2a) select
-; 3) select on Index IndexLink SelectLink
-; 4) IncrementLinks link cog-inc-value!
-
-
-(define (dobs-txt PLAIN-TEXT)
-	(define parses (cog-execute!
-		(Decimate
-			(Number 1)
-			(Decimate
-				(Number 0 1)
-				(LgParseBonds (Phrase PLAIN-TEXT) DICT NUML)))))
-	parses
-)
-
-(define (obs-txt PLAIN-TEXT)
-	(define parses (cog-execute!
-		(Decimate
-			(Number 0 1 1 1 1)
-			; (PureExec
-				(LgParseBonds (Phrase PLAIN-TEXT) DICT NUML)))))
-	(define temp-as (cog-set-atomspace! base-as))
-
-
+; 2) FilterLink with LinkValue signature
+; 3) IncrementLinks link cog-inc-value!
 
 
 ; (obs-txt text-blob)
