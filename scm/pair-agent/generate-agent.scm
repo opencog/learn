7a376d1821430d6e9818ad88e7e2a19889246dcf
Linas Vepštas <linasvepstas@gmail.com>
2024-04-17 16:40:33 -0500

    More theorizing on how to create pairs.

--- scm/pair-agent/generate-agent.scm
+++ scm/pair-agent/generate-agent.scm
@@ -17,5 +17,15 @@
 ; * Allow dict lookup of words according to desired disjuncts.
 ;
 ;
+OK, screw that. The diary part ten is exploring this, here are the best
+ideas in summary form:
+
+1) Starting with a word A, use incoming set to randonly select some
+subset of all possible edges. This will be called the "focus set" or the
+"attentional focus". Because it is small, it can be exhaustively
+iterated and sorted into high-to-low MI order. This provides a weighted
+distribution, from which a rescaled uniform-random choice can be made.
+
+2) Random sample N out of full incomng set.
 
 
